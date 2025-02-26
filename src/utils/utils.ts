fb5d0efe43a81533a492d2c431e24ad6362dd328
Sunab <67991569+sypase@users.noreply.github.com>
2025-02-26 08:44:16 +0545

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,7 @@
 // const serverURL = "https://116.203.47.54:8080/"
 // const serverURL = "https://noaigptserver.xyz"
-const serverURL = "http://localhost:8080"
+// const serverURL = "http://localhost:8080"
+const serverURL = "https://api.checkturnitin.com"
 // const serverURL = "https://api.noaigpt.com"
 // const serverURL = "https://development.noaigpt.com"
 const currencySymbol = "₹";
